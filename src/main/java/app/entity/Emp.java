f40c0505d9b4e12afb806df84217d2e14c05074f
han199805 <jaesun989898@gmail.com>
2025-05-14 07:54:07 +0000

    feat: 전체 사원 조회 API 구현

--- src/main/java/app/entity/Emp.java
+++ src/main/java/app/entity/Emp.java
@@ -1,58 +1,40 @@
 package app.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Getter
 @ToString
-@Builder
 @Entity
+@Table(name = "emp")
 public class Emp {
-	@Id
-	@Column(name = "empno")
-	private Integer empno;
-	
-	@Column(name = "ename")
-	private String ename;
-	
-	@Column(name = "job")
-	private String job;
-	
-	@Column(name = "mgr")
-	private Integer mgr;
-	
-	@Column(name = "hiredate")
-	private LocalDate hiredate;
-	
-	@Column(name = "sal")
-	private Double sal;
-	
-	@Column(name = "comm")
-	private Double comm;
-	
-	@ManyToOne
-	@JoinColumn(name = "deptno")
-	private Dept dept;
-	
-	@Builder
-	public Emp(int empno, String ename, String job, Integer mgr, LocalDate hiredate, Double sal, Double comm, Dept dept) {
-		this.empno = empno;
-		this.ename = ename;
-		this.job = job;
-		this.mgr = mgr;
-		this.hiredate = hiredate;
-		this.sal = sal;
-		this.comm = comm;
-		this.dept = dept;
-	}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "emp_id")
+    private Integer id;
+
+    @Column(name = "emp_name", nullable = false)
+    private String name;
+
+    @Column(name = "emp_position")
+    private String position;
+
+    @Column(name = "emp_salary", precision = 10, scale = 2)
+    private BigDecimal salary;
+
+    @Column(name = "emp_department")
+    private String department;
+
+    @Column(name = "hire_date")
+    private LocalDate hireDate;
+
+    @Column(name = "emp_email", unique = true)
+    private String email;
 }
