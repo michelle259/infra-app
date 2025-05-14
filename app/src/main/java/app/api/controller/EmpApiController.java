1470d01f5748f0d40e996044f1f5c46463e3ff05
wkdjh <wkdjh1019@gmail.com>
2025-05-14 16:47:03 +0900

    add feature : post

--- app/src/main/java/app/api/controller/EmpApiController.java
+++ app/src/main/java/app/api/controller/EmpApiController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
